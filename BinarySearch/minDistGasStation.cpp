abe2a17bacd22dad5399ed95d61aa3f8df1e96e7
shivam980398 <shivayadu89@gmail.com>
2025-07-13 01:08:05 +0530

    Day4-->OptimalAprroach--BS

--- BinarySearch/minDistGasStation.cpp
+++ BinarySearch/minDistGasStation.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 // Brute Force Approach
+//  TC--> O(k * n)
+// SC--> O(n)
 int minDistance(vector<int> &stations, int k, int n)
 {
     vector<int> howMany(n - 1, 0);
@@ -33,6 +35,9 @@ int minDistance(vector<int> &stations, int k, int n)
     return maxAns;
 }
 
+// Better Approach using Priority Queue
+//  TC--> O(k * log(n))
+// SC--> O(n)
 int minDist(vector<int> &stations, int k, int n)
 {
     priority_queue<pair<long double, int>> pq;
@@ -54,12 +59,54 @@ int minDist(vector<int> &stations, int k, int n)
     }
     return pq.top().first;
 }
+
+// Optimal Approach using Binary Search
+//  TC--> O(n * log(maxDistance))
+// SC--> O(1)
+
+int noOfGasStationsRequired(long double mid, vector<int> &stations)
+{
+    int count = 0;
+    for (int i = 1; i < stations.size(); i++)
+    {
+        int noInBetween = (stations[i] - stations[i - 1]) / mid;
+        if (stations[i] - stations[i - 1] / mid == noInBetween * mid)
+        {
+            noInBetween--;
+        }
+        count += noInBetween;
+    }
+    return count;
+}
+int minDistOptimal(vector<int> &stations, int k, int n)
+{
+    long double low = 0, high = 0;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        high = max(high, (long double)(stations[i + 1] - stations[i]));
+    }
+
+    long double diff = 1e-6;
+    while (high - low > diff)
+    {
+        long double mid = (low + high) / 2.0;
+        int count = noOfGasStationsRequired(mid, stations);
+        if (count > k)
+            low = mid;
+
+        else
+            high = mid;
+    }
+    return high;
+}
 int main()
 {
     vector<int> stations = {1, 13, 17, 23};
-    // vector<int> stations = {1, 7};
+    vector<int> stations = {1, 7};
     int k = 5; // Number of gas stations to be added
     int n = stations.size();
     // cout << minDistance(stations, k, n);
-    cout << minDist(stations, k, n);
+    // cout << minDist(stations, k, n);
+    cout << minDistOptimal(stations, k, n);
 }
